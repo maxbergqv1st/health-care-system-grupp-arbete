605cc7f1caac6636c1aab2b03051a5e4aceeed15
github username <dusan.todorovic@student.nbi-handelsakademin.se>
2025-10-13 10:48:47 +0200

    Update

--- Patient-Class.cs
+++ Patient-Class.cs
@@ -2,11 +2,11 @@ namespace App;
 
 class Patient : IUser
 {
-    public Stream Name;
-    public Stream Email;
-    string _password;
+    public string Name;
+    public string Email;
+    public string _password;
 
-    public Patient(string name, string email, string _password)
+    public Patient(string name, string email, string password)
     {
         Name = name;
         Email = email;
@@ -16,6 +16,6 @@ class Patient : IUser
     public bool TryLogin(string username, string password)
     {
         return username == Email && password == _password;
-        
+
     }
 }
