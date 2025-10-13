7bd9f64029a08533915da7c25c0d5bb9da37031f
smilefaceea <lukas.helring@gmail.com>
2025-10-13 11:09:00 +0200

    changed

--- enum.cs
+++ enum.cs
@@ -15,13 +15,3 @@ public enum Regionhalland
 {
     
 }
-
-public class User
-{
-    public string Username { get; set; }
-    private string _password = "test";
-    public bool TryLogin(string username, string password)
-    {
-        return username == Username && password == _password;
-    }
-}
