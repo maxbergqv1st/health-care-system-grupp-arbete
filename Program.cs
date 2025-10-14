4eb4fcb434ccd58ac41d074e3984fd5a10343542
Max Bergqvist <max.bergqvist@student.nbi-handelsakademin.se>
2025-10-14 10:33:52 +0200

    Broke when merge, fixed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ bool found = false;
 bool running = true;
 IUser active_user = null;
 
-while(running)
+while (running)
 {
       if (active_user == null)
       {
@@ -17,117 +17,9 @@ while(running)
             Console.WriteLine("Login [1]\nRegister admin [2]\nRegister doctor [3]\nRegister patient [4]\nQuit [5]\n");
             switch (Console.ReadLine())
             {
-                case "1":
-                    {
-                        Console.Clear();
-                        Console.WriteLine("----------");
-                        Console.WriteLine("Username: ");
-                        string input_username = Console.ReadLine();
-                        Console.WriteLine("Password: ");
-                        string input_password = Console.ReadLine();
-                        foreach (IUser user in users)
+                  case "1":
                         {
-                            if (user.TryLogin(input_username, input_password))
-                            {
-                                    active_user = user;
-                                    found = true;
-                                    break;
-                            }
-                        }
-                        if (found = false)
-                        {
-                            Console.WriteLine("User wasnt found...");
-                        }
-                    }
-                    break;
-                case "2":
-                    {
-                        Console.Clear();
-                        Console.WriteLine("----- Register -----");
-                        Console.WriteLine("Name: ");
-                        string input_name = Console.ReadLine();
-                        Console.WriteLine("Username: ");
-                        string input_username = Console.ReadLine();
-                        Console.WriteLine("Password: ");
-                        string input_password = Console.ReadLine();
-                        Console.WriteLine("Region: North, East, West, South");
-                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
-                        users.Add(new Admin(input_name, input_username, input_password, parsedRegion));
-                        userSystem.SaveUser(users);
-                    }
-                    break;
-                case "3":
-                    {
-                        Console.Clear();
-                        Console.WriteLine("----- Register -----");
-                        Console.WriteLine("Name: ");
-                        string input_name = Console.ReadLine();
-                        Console.WriteLine("Username: ");
-                        string input_username = Console.ReadLine();
-                        Console.WriteLine("Password: ");
-                        string input_password = Console.ReadLine();
-                        Console.WriteLine("Region: North, East, West, South");
-                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
-                        users.Add(new Doctor(input_name, input_username, input_password, parsedRegion));
-                        userSystem.SaveUser(users);
-                    }
-                    break;
-                case "4":
-                    {
-                        Console.Clear();
-                        Console.WriteLine("----- Register -----");
-                        Console.WriteLine("Name: ");
-                        string input_name = Console.ReadLine();
-                        Console.WriteLine("Username: ");
-                        string input_username = Console.ReadLine();
-                        Console.WriteLine("Password: ");
-                        string input_password = Console.ReadLine();
-                        Console.WriteLine("Region: North, East, West, South");
-                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
-                        users.Add(new Patient(input_name, input_username, input_password, parsedRegion));
-                        userSystem.SaveUser(users);
-                    }
-                    break;
-                case "5":
-                    {
-                        running = false;
-                    }
-                    break;
-                default:
-                    continue;
-                    
-            }
-            Console.ReadLine();
-    }
-    else if (active_user.GetType().Name == "Patient")
-    {
-        Console.Clear();
-        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
-        Console.WriteLine("Logged in as Patient");
-        Console.WriteLine("profile\nlogout");
-        Console.ReadLine();
-    }
-    else if (active_user.GetType().Name == "Doctor")
-    {
-        Console.Clear();
-        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
-        Console.WriteLine("Logged in as Doctor");
-        Console.ReadLine();
-    }
-    else if(active_user.GetType().Name == "Admin")
-    {
-        Console.Clear();
-        //MACKISH ADMIN
-        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
-        Console.WriteLine("Logged in as Admin");
-        Console.ReadLine();
-    }
-  
-            Console.WriteLine("login\n[admin] [patient] [doctor] register\nadmin\nquit");
-            switch (Console.ReadLine())
-            {
-                  case "login":
-                        {
+                              Console.Clear();
                               Console.WriteLine("----------");
                               Console.WriteLine("Username: ");
                               string input_username = Console.ReadLine();
@@ -148,8 +40,9 @@ while(running)
                               }
                         }
                         break;
-                  case "admin":
+                  case "2":
                         {
+                              Console.Clear();
                               Console.WriteLine("----- Register -----");
                               Console.WriteLine("Name: ");
                               string input_name = Console.ReadLine();
@@ -163,8 +56,9 @@ while(running)
                               userSystem.SaveUser(users);
                         }
                         break;
-                  case "doctor":
+                  case "3":
                         {
+                              Console.Clear();
                               Console.WriteLine("----- Register -----");
                               Console.WriteLine("Name: ");
                               string input_name = Console.ReadLine();
@@ -178,8 +72,9 @@ while(running)
                               userSystem.SaveUser(users);
                         }
                         break;
-                  case "patient":
+                  case "4":
                         {
+                              Console.Clear();
                               Console.WriteLine("----- Register -----");
                               Console.WriteLine("Name: ");
                               string input_name = Console.ReadLine();
@@ -193,11 +88,14 @@ while(running)
                               userSystem.SaveUser(users);
                         }
                         break;
-                  case "quit":
+                  case "5":
                         {
                               running = false;
                         }
                         break;
+                  default:
+                        continue;
+
             }
             Console.ReadLine();
       }
@@ -205,7 +103,7 @@ while(running)
       {
             Console.Clear();
             // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
-            Console.WriteLine("Logged in as PAtient");
+            Console.WriteLine("Logged in as Patient");
             Console.WriteLine("profile\nlogout");
             Console.ReadLine();
       }
@@ -216,7 +114,7 @@ while(running)
             Console.WriteLine("Logged in as Doctor");
             Console.ReadLine();
       }
-      else if(active_user.GetType().Name == "Admin")
+      else if (active_user.GetType().Name == "Admin")
       {
             Console.Clear();
             //MACKISH ADMIN
@@ -224,4 +122,4 @@ while(running)
             Console.WriteLine("Logged in as Admin");
             Console.ReadLine();
       }
-} 
+}
