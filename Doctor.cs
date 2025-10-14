93d4d5fa68af713c242a999ca5c31993ed348b51
Kevin Diyabanza <kevindiyab@Kevins-MacBook-Air.local>
2025-10-14 10:19:09 +0200

    Made numbers for the menu, Console.Clear() and a default

--- Doctor.cs
+++ Doctor.cs
@@ -2,13 +2,17 @@ namespace App;
 
 class Doctor : IUser
 {
-    public string Username;
-    public string _password;
+    public string Name { get; set; } = ""; //Tomt namn för admin, måste ha ett namn för att hantera data till users.txt.
+    public string Username { get; set; }
+    public string _password { get; set; }
+    public Region UserStatus { get; set; }
 
-    public Doctor(string username, string password)
+    public Doctor(string name, string username, string password, Region region)
     {
+        Name = name;
         Username = username;
         _password = password;
+        UserStatus = region;
     }
     
     public bool TryLogin(string username, string password) //Metod för att använda Account till IUsern loggin.
--- Program.cs
+++ Program.cs
@@ -1,2 +1,125 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using App;
+
+SaveUserSystem userSystem = new();
+List<IUser> users = userSystem.LoadUser();
+
+bool found = false;
+bool running = true;
+IUser active_user = null;
+
+while(running)
+{
+      if (active_user == null)
+      {
+            // ACTIVE USER = INTE HITTAD.
+            Console.Clear();
+            Console.WriteLine("----- Menu -----");
+            Console.WriteLine("Login [1]\nRegister admin [2]\nRegister doctor [3]\nRegister patient [4]\nQuit [5]\n");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----------");
+                        Console.WriteLine("Username: ");
+                        string input_username = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string input_password = Console.ReadLine();
+                        foreach (IUser user in users)
+                        {
+                            if (user.TryLogin(input_username, input_password))
+                            {
+                                    active_user = user;
+                                    found = true;
+                                    break;
+                            }
+                        }
+                        if (found = false)
+                        {
+                            Console.WriteLine("User wasnt found...");
+                        }
+                    }
+                    break;
+                case "2":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----- Register -----");
+                        Console.WriteLine("Name: ");
+                        string input_name = Console.ReadLine();
+                        Console.WriteLine("Username: ");
+                        string input_username = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string input_password = Console.ReadLine();
+                        Console.WriteLine("Region: North, East, West, South");
+                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
+                        users.Add(new Admin(input_name, input_username, input_password, parsedRegion));
+                        userSystem.SaveUser(users);
+                    }
+                    break;
+                case "3":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----- Register -----");
+                        Console.WriteLine("Name: ");
+                        string input_name = Console.ReadLine();
+                        Console.WriteLine("Username: ");
+                        string input_username = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string input_password = Console.ReadLine();
+                        Console.WriteLine("Region: North, East, West, South");
+                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
+                        users.Add(new Doctor(input_name, input_username, input_password, parsedRegion));
+                        userSystem.SaveUser(users);
+                    }
+                    break;
+                case "4":
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----- Register -----");
+                        Console.WriteLine("Name: ");
+                        string input_name = Console.ReadLine();
+                        Console.WriteLine("Username: ");
+                        string input_username = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        string input_password = Console.ReadLine();
+                        Console.WriteLine("Region: North, East, West, South");
+                        Enum.TryParse(Console.ReadLine(), out Region parsedRegion); //Enum.TryParse(Console.ReadLine(), true, out Region parsedRegion);
+                        users.Add(new Patient(input_name, input_username, input_password, parsedRegion));
+                        userSystem.SaveUser(users);
+                    }
+                    break;
+                case "5":
+                    {
+                        running = false;
+                    }
+                    break;
+                default:
+                    continue;
+                    
+            }
+            Console.ReadLine();
+    }
+    else if (active_user.GetType().Name == "Patient")
+    {
+        Console.Clear();
+        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
+        Console.WriteLine("Logged in as Patient");
+        Console.WriteLine("profile\nlogout");
+        Console.ReadLine();
+    }
+    else if (active_user.GetType().Name == "Doctor")
+    {
+        Console.Clear();
+        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
+        Console.WriteLine("Logged in as Doctor");
+        Console.ReadLine();
+    }
+    else if(active_user.GetType().Name == "Admin")
+    {
+        Console.Clear();
+        //MACKISH ADMIN
+        // ACTIVE USER = HITTAD USER OCH MAN ÄR INLOGGAD
+        Console.WriteLine("Logged in as Admin");
+        Console.ReadLine();
+    }
+} 
