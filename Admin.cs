963af703fe306f7419a0a9c7969af1736f0a9643
Marcusjk9 <marcus.jakobsson@student.nbi-handelsakademin.se>
2025-10-16 10:54:10 +0200

    added admin fetures. Verify not a robot

--- Admin.cs
+++ Admin.cs
@@ -21,6 +21,7 @@ class Admin : IUser
   public string _password { get; set; }
   public Region UserStatus { get; set; }
 
+
   List<AdminPermission> permissions = new();
   public Admin(string name, string username, string password, Region region)
   {
@@ -34,6 +35,28 @@ class Admin : IUser
   {
     return username == Username && password == _password; //Skickar username och password till metoden. Returnar true eller false.
   }
+
+  public bool HasPermission(AdminPermission permission)
+  {
+    return permissions.Contains(permission);
+  }
+
+  public void GrantPermission(AdminPermission permission)
+  {
+    if (!permissions.Contains(permission))
+      permissions.Remove(permission);
+  }
+
+  public void RevokePermission(AdminPermission permission)
+  {
+    permissions.Remove(permission);
+  }
+
+  public List<AdminPermission> GetAdminPermissions()
+  {
+    return permissions;
+  }
+
 }
 
 /*
